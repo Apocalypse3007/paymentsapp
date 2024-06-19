e07fe70b04808a759e4c3d666b058448008d10b7
Apocalypseong <94913214+Apocalypse3007@users.noreply.github.com>
2024-06-19 22:19:37 +0530

    frontend progressed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,25 @@
-function App() {
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from "react-router-dom";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
+import { Dashboard } from "./pages/Dashboard";
+import { Money } from "./pages/Money";
 
+function App() {
   return (
-    <div>
-        
-    </div>
+    <>
+       <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<Money />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
